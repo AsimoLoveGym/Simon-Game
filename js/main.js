1827864ff11ca3c9f50de8bc415962e183b3f7e9
AsimoLoveGym <xiazhuangz@gmail.com>
2016-10-24 21:44:54 -0500

    fix: solve the issue of animation break and pointer cursor Todo: more issue to fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Global variables
 var gamePattern = [];
-var clickingFlag = false;
+
 var timeOut = 0;
 var green = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
 var red = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
@@ -9,6 +9,7 @@ var yellow = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
 
 var error = new Audio("http://soundbible.com/mp3/Kick-SoundBible.com-1331196005.mp3");
 
+var clickingFlag = false;
 var expectClickIndex = 0;
 
 
@@ -20,39 +21,39 @@ var simon = function() {
 }
 
 var lightUpAndSoundPlay = function(lightNum) {
-  console.log(lightNum);
+  // console.log(lightNum);
   switch (lightNum) {
     case 0:
-      console.log("Green should light upp");
+      // console.log("Green should light upp");
       $("#green-block").addClass("green-light-up");
       green.play();
       timeOut = setTimeout(function(){
         $("#green-block").removeClass("green-light-up", 400);
-      }, 1000);
+      }, 500);
       break;
     case 1:
-    console.log("Red should light upp");
+    // console.log("Red should light upp");
       $("#red-block").addClass("red-light-up");
       red.play();
       timeOut = setTimeout(function(){
         $("#red-block").removeClass("red-light-up", 400);
-      }, 1000);
+      }, 500);
       break;
     case 2:
-      console.log("Yellow should light upp");
+      // console.log("Yellow should light upp");
       $("#yellow-block").addClass("yellow-light-up");
       yellow.play();
       timeOut = setTimeout(function(){
         $("#yellow-block").removeClass("yellow-light-up", 400);
-      }, 1000);
+      }, 500);
       break;
     case 3:
-      console.log("Blue should light upp");
+      // console.log("Blue should light upp");
       $("#blue-block").addClass("blue-light-up");
       blue.play();
       timeOut = setTimeout(function(){
         $("#blue-block").removeClass("blue-light-up", 400);
-      }, 1000);
+      }, 500);
       break;
   }
 }
@@ -81,13 +82,13 @@ $(document).ready(function(){
 
     lightUpAndSoundPlay(clickedBlockNum);
 
-    console.log("clickedBlockNum",clickedBlockNum);
-    console.log("gamePattern[expectClickIndex]",gamePattern[expectClickIndex]);
+    // console.log("clickedBlockNum",clickedBlockNum);
+    // console.log("gamePattern[expectClickIndex]",gamePattern[expectClickIndex]);
 
     if(clickedBlockNum === gamePattern[expectClickIndex]) {
-      console.log("Correct");
-      console.log("expectClickIndex",expectClickIndex);
-      console.log("gamePattern.length-1",gamePattern.length-1);
+      // console.log("Correct");
+      // console.log("expectClickIndex",expectClickIndex);
+      // console.log("gamePattern.length-1",gamePattern.length-1);
       if(expectClickIndex === (gamePattern.length-1)) {
         clickingFlag = false;
         expectClickIndex = 0;
@@ -97,9 +98,10 @@ $(document).ready(function(){
         expectClickIndex ++;
       }
     } else {
-      console.log("Wrong");
+      // console.log("Wrong");
       error.play();
       clickingFlag = false;
+      expectClickIndex = 0;
       $(".light-block").removeClass("activated");
       playEntirePattern();
     }
@@ -107,7 +109,7 @@ $(document).ready(function(){
 });
 
 var playEntirePattern = function(){
-  console.log("gamePattern: ",gamePattern);
+  // console.log("gamePattern: ",gamePattern);
   var playTime = 0;
   gamePattern.forEach(function(item, index, array){
     timeOut = setTimeout(function(){
@@ -117,9 +119,9 @@ var playEntirePattern = function(){
         timeOut = setTimeout(function(){
           clickingFlag = true;
           $(".light-block").addClass("activated");
-        }, array.length * 1000);
+        }, array.length * 1500);
       }
-    }, (index+1)*1000)
+    }, (index+1)*1500);
   })
 }
 
